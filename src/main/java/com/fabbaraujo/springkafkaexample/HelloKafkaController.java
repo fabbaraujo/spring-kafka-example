f2cbac0fb78abcba5eea44340f301c1019718c78
Fabrício Araújo <fabaraujo23@gmail.com>
2020-11-27 14:45:06 -0300

    Recebendo messages via JSON, String e byteArray

--- src/main/java/com/fabbaraujo/springkafkaexample/HelloKafkaController.java
+++ src/main/java/com/fabbaraujo/springkafkaexample/HelloKafkaController.java
@@ -1,15 +1,20 @@
 package com.fabbaraujo.springkafkaexample;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.header.Headers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
 
 @RestController
 public class HelloKafkaController {
@@ -44,5 +49,34 @@ public class HelloKafkaController {
         logger.info("All messages received");
 
         return "Hello Kafka!";
+    }
+
+    @KafkaListener(topics = "advice-topic", clientIdPrefix = "json", containerFactory = "kafkaListenerContainerFactory")
+    public void listenAsObject(ConsumerRecord<String, PracticalAdvice> cr, @Payload PracticalAdvice payload) {
+        logger.info("Logger 2 [String] received key {}: Type [{}] | Payload: {} | Record: {}", cr.key(),
+                typeIdHeader(cr.headers()), payload, cr.toString());
+        latch.countDown();
+    }
+
+    @KafkaListener(topics = "advice-topic", clientIdPrefix = "string", containerFactory = "kafkaListenerStringContainerFactory")
+    public void listenAsString(ConsumerRecord<String, String> cr, @Payload String payload) {
+        logger.info("Logger 2 [String] received key {}: Type [{}] | Payload: {} | Record: {}", cr.key(),
+                typeIdHeader(cr.headers()), payload, cr.toString());
+        latch.countDown();
+    }
+
+
+    @KafkaListener(topics = "advice-topic", clientIdPrefix = "bytearray", containerFactory = "kafkaListenerByteArrayContainerFactory")
+    public void listenAsByteArray(ConsumerRecord<String, byte[]> cr, @Payload byte[] payload) {
+        logger.info("Logger 2 [String] received key {}: Type [{}] | Payload: {} | Record: {}", cr.key(),
+                typeIdHeader(cr.headers()), payload, cr.toString());
+        latch.countDown();
+    }
+
+
+    private static String typeIdHeader(Headers headers) {
+        return StreamSupport.stream(headers.spliterator(), false)
+                .filter(header -> header.key().equals("__TypeId__"))
+                .findFirst().map(header -> new String(header.value())).orElse("N/A");
     }
 }

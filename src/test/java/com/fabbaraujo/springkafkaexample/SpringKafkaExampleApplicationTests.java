c465360dd15f46cb2115ef4a51d538fc66ff3c76
Fabrício Araújo <fabaraujo23@gmail.com>
2020-11-27 13:48:26 -0300

    Adicionando dockerfile e properties

---
+++ src/test/java/com/fabbaraujo/springkafkaexample/SpringKafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabbaraujo.springkafkaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
